f66552fe2e8224fbbbab02804e244a67308531ab
hanifb1360 <hanif.bahari@gmail.com>
2022-07-29 22:13:15 +0200

    first

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./app.css";
+import {Users} from "./users";
+
+function App() {
+  
+  const [query, setQuery] = useState("");
+    
+  console.log(query)
+  return (
+    <div className="app">
+        <input type="text" placeholder="Search..." className="search" onChange={(e) => setQuery(e.target.value)}/>
+        <ul className="list">
+        {Users.map((user) =>(
+            <li key={user.id} className="listItem">{user.first_name}</li>
+        ))}
+        
+        
+
+        </ul>
+    </div>
+  )
+}
+
+export default App
