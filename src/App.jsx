692a366dad5177280351e00d0566640951287501
hanifb1360 <hanif.bahari@gmail.com>
2022-08-04 23:12:44 +0200

    2

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,16 @@ function App() {
   
   const [query, setQuery] = useState("");
     
-  console.log(query)
+  
   return (
     <div className="app">
         <input type="text" placeholder="Search..." className="search" onChange={(e) => setQuery(e.target.value)}/>
         <ul className="list">
-        {Users.map((user) =>(
+        {Users.filter((user) => 
+         user.first_name.toLowerCase().includes(query))
+         .map((user) =>(
             <li key={user.id} className="listItem">{user.first_name}</li>
         ))}
-        
-        
-
         </ul>
     </div>
   )
